837b84053da09820cf5495c0260beb745e8ee5fe
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-11-22 15:03:16 -0800

    Add files via upload

--- LilT00tz/RollDice.js
+++ LilT00tz/RollDice.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import Die from './Die';
 
 class RollDice extends Component {
+    static defaultProps = {
+        sides: ["one", "two", "three", "four", "five", "six"]
+    };
     constructor(props){
         super(props);
         this.state = {die1: "one", die2: "one"};
+        this.roll = this.roll.bind(this);
+    }
+    roll() {
+        const newDie1 = this.props.sides[
+            Math.floor(Math.random() * this.props.sides.length)
+        ];
+        const newDie2 = this.props.sides[
+            Math.floor(Math.random() * this.props.sides.length)
+        ];
+        this.setState({ die1: newDie1, die2: newDie2, rolling: true });
+
     }
     render() {
         return (
-            <div>
+            <div clasName="RollDice">
                 <Die face={this.state.die1} />
                 <Die face={this.state.die2} />
+                <button onCLick={this.roll}>Roll Dice!</button>
             </div>
         );
     }

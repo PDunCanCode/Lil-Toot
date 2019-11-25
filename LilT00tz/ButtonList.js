8aa59141eb5be7d68bd90de52977edff051fd810
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-11-25 11:49:21 -0800

    Add files via upload

--- LilT00tz/ButtonList.js
+++ LilT00tz/ButtonList.js
@@ -5,13 +5,23 @@ class ButtonList extends Component {
     static defaultProps = {
         colors: ["#e056fd", "#eb4d4b", "#badc58", "#f0932b"]
     };
+    constructor(props) {
+        super(props);
+        this.state = { color: "cyan" }
+    }
+
+    changeColor(newColor){
+        this.setState({ color: newColor });
+    }
 
     render() {
         return (
             <div ClassName='ButtonList'>
                 {this.props.colors.map(c => {
-                    const colorObj = { bacjgroundColor: c };
-                    return <button style={colorObj}>Click Me!</button>
+                    const colorObj = { backgroundColor: c };
+                    return <button style={colorObj} onClick={() => this.changeColor(c)}>
+                         Click Me!
+                         </button>
                 })}
             </div>
         );

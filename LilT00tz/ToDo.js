0a758bd5f58a396eadcf15bc6fb0dc00c1e6ceaa
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-11-28 19:50:28 -0800

    Add files via upload
    ToDo.js major refactor, using hooks. (Delete / remove working)

--- LilT00tz/ToDo.js
+++ LilT00tz/ToDo.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import useToggleState from "./hooks/useToggleState";
 import EditTodoForm from "./EditTodoForm";
@@ -12,9 +13,38 @@ import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 function Todo({ id, task, completed, removeTodo, toggleTodo, editTodo }) {
   const [isEditing, toggle] = useToggleState(false);
   return (
-    <ListItem style={{ height: "64px" }}></ListItem>
-
-
+    <ListItem style={{ height: "64px" }}>
+      {isEditing ? (
+        <EditTodoForm
+          editTodo={editTodo}
+          id={id}
+          task={task}
+          toggleEditForm={toggle}
+        />
+      ) : (
+        <>
+          <Checkbox
+            tabIndex={-1}
+            checked={completed}
+            onClick={() => toggleTodo(id)}
+          />
+          <ListItemText
+            style={{ textDecoration: completed ? "line-through" : "none" }}
+          >
+            {task}
+          </ListItemText>
+          <ListItemSecondaryAction>
+            <IconButton aria-label='Delete' onClick={() => removeTodo(id)}>
+              <DeleteIcon />
+            </IconButton>
+            <IconButton aria-label='Edit' onClick={toggle}>
+              <EditIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </>
+      )}
+    </ListItem>
   );
+}
 
-export default Todo
+export default Todo;

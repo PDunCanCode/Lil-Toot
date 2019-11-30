a7dbbe1b1e1ec9d3765f510883a12d0d0d986029
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-11-29 16:03:27 -0800

    T.McG Hooks
    Full  CRUD functionality. (working)

--- LilT00tz/toDoSimple.js
+++ LilT00tz/toDoSimple.js
@@ -1,8 +1,22 @@
 import React, { useState} from 'react';
+function generateId() {
+    return '_' = Math.random().toString(36).substr(2, 9);
+}
 
 function ToDo () {
     const [todos, setTodos] = React.useState([])
     const [input, setInput] = React.useState('')
+
+    const handleSubmit = () => {
+        setTodos((todos) => todos.concat({
+            text:input,
+            id: generateId()
+        }))
+
+        setInput('')
+    }
+
+    const removeTodo = (id) => setTodos((todos) => todos.filter((t) =>t.id !==id))
     
     return (
         <div>
@@ -10,8 +24,18 @@ function ToDo () {
             type='text'
             value={input}
             placeholder="New Todo"
-            onChange=((e) => setInput())
+            onChange={(e) => setInput(e.target.value)}
             />
+            <button onClick={handleSubmit}>Submit</button>
+
+            <ul>
+                {todos.map(({ text, id }) => (
+                    <li key ={id}>
+                        <span>{text}</span>
+                        <button onClick={() => removeTodo(id)}>X</button>
+                    </li>
+                ))}
+            </ul>
         </div>
 
     )

551b4765e78ae0c28aa776fecdc2fa05fc565802
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-12-01 14:10:20 -0800

    The kajigger is Kajigging

--- LilT00tz/useEffectProf.js
+++ LilT00tz/useEffectProf.js
@@ -1,6 +1,34 @@
 import React, { useState, useEffect } from "react"
 import ReactDOM from "react-dom"
 
+function Profile (username) {
+  const [profile, setProfile] = React.useState(null)
+
+  React.useEffect(() => {
+    getGitHubProfile('username').then(setProfile)
+  }, [])
+  
+  fetch(`https://api.github.com/users/${username}`)
+    .then((res) => res.json())
+}
+
+function Profile() {
+  return (
+    <div>
+      <p>getGithubProfile will return an object full of properties 
+        related to the profile of whatever username you pass in as the first argument</p>
+
+      <p>Here's a sample response with the "username" of "tylermcginnis"</p>
+      <pre>
+        {JSON.stringify({
+          login: 'tylermcginnis',
+          avatar_url: 'https://avatars3.githubusercontent.com/u/2933430?v=4',
+          bio: 'Educating at TylerMcGinnis.com. Google Developer Expert. Previous Director at DevMountain.'
+        }, null, 2)}
+      </pre>
+    </div>
+  );
+}
 
 
 const rootElement = document.getElementById("root");

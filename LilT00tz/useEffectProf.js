408129abe8e744d50e3857527c7381bf29c32750
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-11-30 16:08:56 -0800

    useState && useEffect practice
    refactored code to implement hooks, working - Ka-CHOW

--- LilT00tz/useEffectProf.js
+++ LilT00tz/useEffectProf.js
@@ -6,28 +6,24 @@ function Profile () {
 
   React.useEffect(() => {
     getGithubProfile ('tylermcginnis').then(setProfile)
-  })
-  return fetch(`https://api.github.com/users/${username}`)
-    .then((res) => res.json())
-}
+  }, [])
+  
+  if (profile === null) {
+    return <p>Loading...</p>
+  }
 
-function Profile() {
   return (
-    <div>
-      <p>getGithubProfile will return an object full of properties 
-        related to the profile of whatever username you pass in as the first argument</p>
-
-      <p>Here's a sample response with the "username" of "tylermcginnis"</p>
-      <pre>
-        {JSON.stringify({
-          login: 'tylermcginnis',
-          avatar_url: 'https://avatars3.githubusercontent.com/u/2933430?v=4',
-          bio: 'Educating at TylerMcGinnis.com. Google Developer Expert. Previous Director at DevMountain.'
-        }, null, 2)}
-      </pre>
-    </div>
+    <React.Fragment>
+      <h1>@{profile.login}</h1>
+      <img 
+        src={profile.avatar_url} 
+        alt={`Avatar for ${profile.login}`} 
+      />
+      <p>{profile.bio}</p>
+    </React.Fragment>
   );
 }
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<Profile />, rootElement);

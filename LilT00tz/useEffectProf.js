0615a81f6e4ee5817f4363ed19e53e59fd30fe95
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-12-01 14:02:10 -0800

    Add files via upload

--- LilT00tz/useEffectProf.js
+++ LilT00tz/useEffectProf.js
@@ -1,28 +1,6 @@
 import React, { useState, useEffect } from "react"
 import ReactDOM from "react-dom"
 
-function Profile () {
-  const [profile, setProfile] = React.useState(null)
-
-  React.useEffect(() => {
-    getGithubProfile ('tylermcginnis').then(setProfile)
-  }, [])
-  
-  if (profile === null) {
-    return <p>Loading...</p>
-  }
-
-  return (
-    <React.Fragment>
-      <h1>@{profile.login}</h1>
-      <img 
-        src={profile.avatar_url} 
-        alt={`Avatar for ${profile.login}`} 
-      />
-      <p>{profile.bio}</p>
-    </React.Fragment>
-  );
-}
 
 
 const rootElement = document.getElementById("root");

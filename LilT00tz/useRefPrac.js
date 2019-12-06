281789b988bec3e4b698f08e61894405a7073abd
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-12-05 16:38:47 -0800

    React.useRef()

--- LilT00tz/useRefPrac.js
+++ LilT00tz/useRefPrac.js
@@ -5,8 +5,8 @@ import "./styles.css";
 
 /*
   Instructions:
-    You're given the UI for a basic form. Your task is to 
-    hook it all up using refs. 
+    You're given the UI for a basic form. Your task is to
+    hook it all up using refs.
 
     The `Focus X Input` buttons should focus that specific input
     field.
@@ -14,17 +14,29 @@ import "./styles.css";
     The `Submit` button should log `name`, `email`, and `password`
     to the console.
 
-    The `Reset` button should result all of the input fields to 
+    The `Reset` button should result all of the input fields to
     empty strings.
 */
 
 function Form() {
-  const handleSubmit = e => {
+  const nameRef = React.useRef()
+  const emailRef = React.useRef()
+  const passwordRef = React.useRef()
 
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const name = nameRef.current.value
+    const email = emailRef.current.value
+    const password = passwordRef.current.value
+
+    console.log(name, email, password)
   }
 
   const handleReset = () => {
-
+    nameRef.current.value = ''
+    emailRef.current.value = ''
+    passwordRef.current.value = ''
   }
 
   return (
@@ -34,6 +46,7 @@ function Form() {
         <input
           placeholder="name"
           type="text"
+          ref={nameRef}
         />
       </label>
       <label>
@@ -41,6 +54,7 @@ function Form() {
         <input
           placeholder="email"
           type="text"
+          ref={emailRef}
         />
       </label>
       <label>
@@ -48,25 +62,26 @@ function Form() {
         <input
           placeholder="password"
           type="text"
+          ref={passwordRef}
         />
       </label>
 
       <hr />
 
-      <button>
+      <button onClick={() => nameRef.current.focus()}>
         Focus Name Input
       </button>
-      <button>
+      <button onClick={() => emailRef.current.focus()}>
         Focus Email Input
       </button>
-      <button>
+      <button onClick={() => passwordRef.current.focus()}>
         Focus Password Input
       </button>
 
       <hr />
 
-      <button>Submit</button>
-      <button>Reset</button>
+      <button onClick={handleSubmit}>Submit</button>
+      <button onClick={handleReset}>Reset</button>
     </React.Fragment>
   )
 }

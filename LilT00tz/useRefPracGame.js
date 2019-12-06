faaf3f4f7e1699e63d8c06548ff75fa09615e15d
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-12-05 19:18:00 -0800

    useRef && useEffect setInterval()

--- LilT00tz/useRefPracGame.js
+++ LilT00tz/useRefPracGame.js
@@ -3,21 +3,35 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-/*
-  Instructions:
-    You're building an app to see how many times you can click
-    a button in 10 seconds. 
-
-    The UI has three parts, a button, a timer counting down from 10,
-    and a count of how many times you've clicked the button.
-
-    Once the timer reaches 0, remove the button from the UI.
-*/
-
 function CounterGame () {
+  const [count, setCount] = React.useState(0)
+  const [timeLeft, setTimeLeft] = React.useState(10)
+  const id = React.useRef(null)
+
+  const clear = () => window.clearInterval(id.current)
+
+  React.useEffect(() => {
+    id.current = window.setInterval(() => {
+      setTimeLeft((time) => time - 1)
+    }, 1000)
+
+    return clear
+  }, [])
+
+  React.useEffect(() => {
+    if (timeLeft === 0) {
+      clear()
+    }
+  }, [timeLeft])
+  
   return (
     <div className="App">
-      See instructions.
+      <h1>{count}</h1>
+      <h3>Time left: {timeLeft} seconds</h3>
+      {timeLeft === 0 ? null : 
+        <button onClick={() => setCount((c) => c + 1)}>
+          +
+        </button>}
     </div>
   );
 }

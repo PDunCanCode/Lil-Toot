452e3cd262bc2c35f0da0fbf6998c27b91ff0e67
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2019-12-10 10:50:59 -0800

    Add files via upload

--- LilT00tz/async-hooks.js
+++ LilT00tz/async-hooks.js
@@ -1,36 +1,65 @@
 import React, { useState, useEffect } from 'react';
 
-async function fetchData(query) {
-    try {
-        const response = await fetch(
-
-        );
-        const json = await response.json();
-    } catch (error) {
-        
-    }
-}
-export default function AsyncHooks() {
-    const [search, setSearch] = useState('');
-    const [query, setQuery] = useState('');
+function useGiphy(query) {
     const [results, setResults] = useState([]);
-
-    useEffect(() => {
-        if (query !== '') {
-            fetchData().then(setResults);
-        }
-    }, {query});
+    const [loading, setLoading] = useState(false);
+}
+useEffect(() => {
+    async function fetchData(query) {
+        try {
+            const response = await fetch(
     
-    return (
-        <div>
-        <h1>Async React Hooks</h1>
-        <form onSubmit>
-            <input
-            value={search}
-            onChange={e=> setSearch(e.target.value)}
-            placeholder= "Search for Gifs" 
-            />
-        </form>
-        </div>
-    );
+            );
+            const json = await response.json();
+            setResults(
+                json.data.map(item => {
+                  return item.images.preview.mp4;
+                })
+              );
+            } finally {
+              setLoading(false);
+            }
+          }
+      
+          if (query !== '') {
+            fetchData();
+          }
+        }, [query]);
+    
+        return [results, loading];
+      }
+    }
+    export default function AsyncHooks() {
+        const [search, setSearch] = useState('');
+        const [query, setQuery] = useState('');
+        const [results, setResults] = useState([]);
+    
+        useEffect(() => {
+            if (query !== '') {
+                fetchData().then(setResults);
+            }
+        }, {query});
+    
+        return (
+            <div>
+            <h1>Async React Hooks</h1>
+            <form onSubmit={e => {
+                e.preventDefault();
+                setQuery(search);
+            }}>
+                <input
+                value={search}
+                onChange={e=> setSearch(e.target.value)}
+                placeholder= "Search for Gifs" 
+                />
+                <button type="submit">Search</button>
+            </form>
+            <br />
+          {loading ? (
+            <h1>GIVE ME GIFS</h1>
+          ) : (
+            results.map(item => <video autoPlay loop key={item} src={item} />)
+          )}
+            </div>
+        );
 }
